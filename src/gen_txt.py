4704937b18104ef358d539e3da48bf1de8106861
Israel Lopez <israel.lopez@ucr.ac.cr>
2021-10-03 11:00:34 -0600

    Re-added debris names to coords file

--- src/gen_txt.py
+++ src/gen_txt.py
@@ -5,12 +5,14 @@ from skyfield.api import load, wgs84, EarthSatellite
 import os
 import shutil
 
+# ---------- GLOBAL CONSTANTS ----------
 wd = "../"  # Where the txt/ directory will be created
 common_name = 'cosmos-2251-debris'  # Name of the txt with the LSE (make sure this matches the file name at Celestrak)
 common_txt_name = common_name + '.txt'  # Common_name + .txt
 txt_path = wd + 'txt/'  # Path of the txt/ directory
 lse_file = txt_path + common_name + '.txt'  # Name of the file with the LSEs
 coords_file = txt_path + common_name + '-coords.txt'  # Name of the file with coordinates
+# --------------------------------------
 
 
 # Creates txt/ dir in working directory
@@ -39,6 +41,7 @@ def gen_coords():
 		gen_LSE()
 
 	counter = 0
+	name = ''
 	l1 = ''
 	l2 = ''
 	debris_list = []
@@ -46,12 +49,13 @@ def gen_coords():
 	f = open(lse_file, 'r')
 	for line in f:  # Loops through the LSE txt file
 		if counter % 3 == 0:
-			if l1 != '':
+			if name != '':
 				debris = EarthSatellite(l1, l2, '', ts)
 				geocentric = debris.at(t)
 				subpoint = wgs84.subpoint(geocentric)
-				coords_str = str(subpoint.latitude.degrees) + ',' + str(subpoint.longitude.degrees) + ',' + str(subpoint.elevation.km)
+				coords_str = name.rstrip() + ',' + str(subpoint.latitude.degrees) + ',' + str(subpoint.longitude.degrees) + ',' + str(subpoint.elevation.km)
 				debris_list.append(coords_str)  # Appends the coords as a string (latitude,longitude,elevation) to a list
+			name = line
 		elif counter % 3 == 1:
 			l1 = line
 		elif counter % 3 == 2:
